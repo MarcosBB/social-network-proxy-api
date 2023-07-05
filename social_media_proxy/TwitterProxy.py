a30f9457c2f5f024d3d41630d58e385d3b3bef6b
MarcosBB <marcosberaldobarros@hotmail.com>
2023-07-05 02:41:17 -0300

    twitter proxy mvp

--- social_media_proxy/TwitterProxy.py
+++ social_media_proxy/TwitterProxy.py
@@ -1,31 +1,11 @@
 # from .Proxy import Proxy
 import tweepy
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 class TwitterProxy():
-    def __init__(self):
+    def __init__(self, bearer_token):
         self.client = tweepy.Client(
-            bearer_token=os.getenv("BEARER_TOKEN"),
-            consumer_key=os.getenv("API_KEY"),
-            consumer_secret=os.getenv("API_KEY_SECRET"),
-            access_token=os.getenv("ACCESS_TOKEN"),
-            access_token_secret=os.getenv("ACCESS_TOKEN_SECRET"),
+            bearer_token=bearer_token
         )
-        
-        # self.auth = tweepy.OAuth2AppHandler(
-        #     os.getenv("API_KEY"), 
-        #     os.getenv("API_KEY_SECRET"),
-        # )
-        
-        # self.auth.set_access_token(
-        #     os.getenv("ACCESS_TOKEN"),
-        #     os.getenv("ACCESS_TOKEN_SECRET"),
-        # )
-
-        # self.api = tweepy.API(self.auth)
     
     def get_me(self):
         return self.client.get_me().data.data
@@ -40,7 +20,11 @@ class TwitterProxy():
         return self.client.delete_tweet(
             id=id,
         ).data
-
-
-twitter = TwitterProxy()
-import pdb; pdb.set_trace()
+    
+    @classmethod
+    def token(consumer_key, consumer_secret):
+        auth = tweepy.OAuth2AppHandler(
+            consumer_key, 
+            consumer_secret,
+        )
+        return {"bearer_token": auth.apply_auth().bearer_token}
